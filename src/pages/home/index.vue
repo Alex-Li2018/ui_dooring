59029f429d896035c97dd7d9a76cc912b76251ff
alex <alexlismile@163.com>
2021-07-12 13:55:17 +0800

    feat: 增加首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,19 +1,131 @@
 <template>
     <div class="home_wrap">
-        首页
+        <div class="top-wrap">
+            <div class="f-screen position-style"></div>
+            <div class="s-screen position-style"></div>
+            <div class="t-screen position-style"></div>
+            <div class="content_wrap">
+                <div class="left">
+                    <div class="title">ui Dooring</div>
+                    <div class="desc">
+                        Create interactive, easy to follow product tours that will help your users get started and adopt new features quicker than ever. No coding or big budgets required
+                    </div>
+                    <div class="btn" @click="jumpUrl">TRY NOW</div>
+                </div>
+                <div class="right">
+                    <svg 
+                        viewBox="0 0 660 430" 
+                        class="d2 k8 k9 ka kb jt" 
+                        style=" 
+                            width:660px;
+                            height:430px;
+                            pointer-events:none;
+                            border-radius:8px"
+                    ><g><g><g><rect x="0" y="0" fill="white" width="660" height="430"></rect><rect x="0" y="0" width="660" height="40" fill="#eaeef4"></rect><rect rx="4" ry="4" x="115.5" y="13" width="429" height="20" fill="#d5dbe3"></rect><rect x="0" y="39" width="660" height="1" fill="#afbbc7"></rect><path transform="translate(47.5,15)" d="M7.2 3L2.7 7.3 7.2 12" stroke="#9a9a9a" fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path transform="translate(82.5,15)" d="M3.2 11.9l4.5-4.5-4.5-4.5" stroke="#9a9a9a" fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path transform="translate(559.5,15)" d="M8 3v10M3 8h10" stroke="#9a9a9a" fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><g transform="translate(589.5,15)"><path d="M9.6 5.4H4.1a1 1 0 00-1 1V12c0 .6.4 1 1 1h5.5c.6 0 1-.4 1-1V6.5c0-.6-.4-1.1-1-1.1z" stroke="#9a9a9a" fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12.2 10.7a.9.9 0 00.8-.8v-6a.9.9 0 00-.8-.8h-6a.9.9 0 00-.8.8" stroke="#9a9a9a" fill="none" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></g><path transform="translate(243.5,18)" fill="#9a9a9a" d="M6.73 3.65v-.88a2.77 2.77 0 00-5.54 0v.88a1.38 1.38 0 00-.69 1.2v2.77A1.38 1.38 0 001.88 9h4.16a1.38 1.38 0 001.38-1.38V4.85a1.38 1.38 0 00-.69-1.2zm-1.38.5H2.58V2.77a1.38 1.38 0 112.77 0v1.38z"></path><rect rx="2" ry="2" x="258.5" y="20" width="143" height="6" fill="#afbbc7"></rect></g>
+                    <svg y="40" width="660" height="390" viewBox="0 0 750 500" preserveAspectRatio="xMinYMin slice"><g fill="#FF9100" fill-rule="evenodd"><g transform="translate(358 335)"><path d="M.3 6h88.2v4.7H.3z"></path><path opacity=".5" d="M.3 18.5h180v4.7H.3zM.3 30.5h45v4.7H.3zM133.3 30.5h55v4.7h-55zM50.3 30.5h78v4.7h-78z"></path><rect opacity=".2" x="272" width="38" height="38" rx="2"></rect><rect opacity=".2" x="228" width="38" height="38" rx="2"></rect><path d="M296 15c0 2.6-2 4.6-4.7 4.6-2.5 0-4.5-2-4.5-4.6 0-2.6 2-4.6 4.5-4.6 2.6 0 4.6 2 4.6 4.6zm-14 11.7c0-3.2 6-5 9-5 3.2 0 9.3 1.8 9.3 5v1.5H282v-1.5zM247.5 28s7.5-7.7 7.5-11.7c0-4-3.4-7.3-7.5-7.3-4 0-7.5 3.3-7.5 7.3s7.5 11.7 7.5 11.7zm0-9.8c1.4 0 2.6-1.2 2.6-2.5 0-1.4-1-2.6-2.5-2.6-1.4 0-2.6 1.3-2.6 2.7 0 1.3 1 2.5 2.5 2.5z" opacity=".8"></path></g><g transform="translate(358 79)"><rect opacity=".2" width="310" height="230" rx="4"></rect><g transform="translate(101 90)"><path opacity=".8" d="M37 1l20.5 34.4L75.7 23l30 35H.3"></path><ellipse opacity=".8" cx="93.1" cy="9.7" rx="8.7" ry="8.7"></ellipse></g></g><g><g transform="translate(83 335)"><path d="M63.3 11.7H129v4.7H63.3z"></path><path opacity=".5" d="M63.3 23.4h129.2V28H63.3zM63.3 35.2h131.3V40H63.3z"></path><rect opacity=".2" width="51.6" height="51.6" rx="2"></rect><g transform="translate(7, 15) scale(0.35)"><path opacity=".8" d="M37 1l20.5 34.4L75.7 23l30 35H.3"></path><ellipse opacity=".8" cx="93.1" cy="9.7" rx="8.7" ry="8.7"></ellipse></g></g><g transform="translate(83 270)"><path d="M63.3 11.7h76.2v4.7H63.3z"></path><path opacity=".5" d="M63.3 23.4H210V28H63.2zM63.3 35.2h129V40h-129z"></path><rect opacity=".2" width="51.6" height="51.6" rx="2"></rect><g transform="translate(7, 15) scale(0.35)"><path opacity=".8" d="M37 1l20.5 34.4L75.7 23l30 35H.3"></path><ellipse opacity=".8" cx="93.1" cy="9.7" rx="8.7" ry="8.7"></ellipse></g></g><g transform="translate(83 205)"><path d="M63.3 10H158v4.7H63.3zM163.6 10h10.8v4.7h-10.8z"></path><path opacity=".5" d="M63.3 21.7h140v4.7h-140zM63.3 33.5h59.2v4.7H63.3z"></path><rect opacity=".2" width="51.6" height="51.6" rx="2"></rect><g transform="translate(7, 15) scale(0.35)"><path opacity=".8" d="M37 1l20.5 34.4L75.7 23l30 35H.3"></path><ellipse opacity=".8" cx="93.1" cy="9.7" rx="8.7" ry="8.7"></ellipse></g></g><g transform="translate(83 140)"><path d="M63.3 11.7h76.2v4.7H63.3z"></path><path opacity=".5" d="M63.3 23.4H210V28H63.2zM63.3 35.2h129V40h-129z"></path><rect opacity=".2" width="51.6" height="51.6" rx="2"></rect><g opacity=".8" transform="translate(13, 13)"><path d="M13 26c7.2 0 13-5.8 13-13S20.2 0 13 0 0 5.8 0 13s5.8 13 13 13zM9 12.6V6l5.7 3.3 5.7 3.3-5.7 3.3L9 19v-6.6z"></path></g></g><g transform="translate(83 75)"><path d="M63.3 10H116v4.7H63.4zM122 10h17.5v4.7H122z"></path><path opacity=".5" d="M63.3 21.7H210v4.7H63.2zM63.3 33.5H210v4.7H63.2z"></path><rect opacity=".2" width="51.6" height="51.6" rx="2"></rect><g opacity=".8" transform="translate(13, 13)"><path d="M13 26c7.2 0 13-5.8 13-13S20.2 0 13 0 0 5.8 0 13s5.8 13 13 13zM9 12.6V6l5.7 3.3 5.7 3.3-5.7 3.3L9 19v-6.6z"></path></g></g></g><path d="M0 0h750v34H0z"></path><path fill="#fff" d="M20.5 8L29 25H12"></path><text dy="0.3em" x="45" y="17" style="font-size: 14px; fill: rgb(255, 255, 255); font-weight: bold;">MySite</text><circle cx="720" cy="17" stroke="#fff" stroke-width="1" r="9"></circle><rect x="600" y="8" width="100" height="18" rx="8" stroke="#fff" stroke-width="1"></rect></g>
+                    </svg>
+                    </g></g></g>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="bottom_wrap" @click="jumplink">© Copyright - Ui Dooring</div>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {};
+        methods: {
+            jumpUrl() {
+                this.$router.push({
+                    path: '/editor'
+                });
+            },
+            jumplink() {
+                window.open('https://beian.miit.gov.cn');
+            }
         }
     };
 </script>
 
 <style lang="scss">
-.home_wrap {
-
+.top-wrap {
+    position: relative;
+    .position-style {
+        position: absolute;
+    }
+    .f-screen {
+        width: 100%;
+        height: 680px;
+        top: -650px;
+        background: linear-gradient(90deg, #2196F3, #1976D2);
+        transform: skewY(-8.5deg) translateY(76%);
+    }
+    .s-screen {
+        width: 100%;
+        height: 600px;
+        background: linear-gradient(100deg, #1976D2, #2196F3);
+        transform: skewY(-8.5deg) translateY(76%);
+        top: -650px;
+    }
+    .t-screen {
+        width: 100%;
+        height: 365px;
+        background: linear-gradient(20deg, #1E88E5, #1976D2);
+        transform: skewY(-8.5deg) translateY(76%);
+        top: -650px;
+    }
+    .content_wrap {
+        padding: 120px 20px 100px 20px;
+        max-width: 1040px;
+        height: 650px;
+        color: #fff;
+        margin: 0 auto;
+        box-sizing: border-box;
+        position: relative;
+        z-index: 3;
+        display: flex;
+        justify-content: space-between;
+        .title {
+            line-height: 1.25;
+            padding-bottom: 30px;
+            letter-spacing: 0.8px;
+            font-weight: 500;
+            font-size: 44px;
+        }
+        .desc {
+            line-height: 1.35;
+            padding-bottom: 50px;
+            font-size: 24px;
+            font-weight: 300;
+            width: 360px;
+        }
+        .btn {
+            background: #ffab40;
+            padding: 10px 30px;
+            border-radius: 5px;
+            box-shadow: 0 4px 6px rgba(50,50,50,10%), 0 1px 3px rgba(255,255,255,10%);; 
+            color: #fff;
+            font-size: 20px;
+            width: 205px;
+            height: 52px;
+            box-sizing: border-box;
+            text-align: center;
+            line-height: 32px;
+            cursor: pointer;
+        }
+        .right {
+            overflow: hidden;
+            transform: scale(0.9) translateX(-25px) translateY(-30px) perspective(3090px) rotateY(-29deg) rotateX(2deg) rotate(1deg);
+            box-shadow: 1px 1px 5px 0 rgba(26,26,67,5%), 39px 62.5px 125px -25px rgba(50,50,93, 50%), 23.4px 37.5px 75px -37.5px rgba(0,0,0,60%);
+        }
+    }
+}
+.bottom_wrap {
+    background-color: #fff;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    cursor: pointer;
 }
 </style>
