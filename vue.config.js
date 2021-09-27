f2d750986c8dae1b4659abe08fe0d782fc7f7ef6
alex <alexlismile@163.com>
2021-09-27 17:33:06 +0800

    dev环境不上传sourcemap

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
 const path = require('path');
 const SentryPlugin = require('@sentry/webpack-plugin');
 
+function getPlugins () {
+    const plugins = [];
+    if (process.env.NODE_ENV === 'production') {
+        plugins.push(
+            new SentryPlugin({
+                include: './dist',
+                release: process.env.RELEASE_VERSION,
+                configFile: 'sentry.properties',
+                urlPrefix: '~/'
+            })
+        );
+    }
+}
+
 module.exports = {
     publicPath: '/',
     outputDir: 'dist',
@@ -34,31 +48,6 @@ module.exports = {
                 '@': path.resolve(__dirname, './src')
             }
         },
-        plugins: [
-            // ...省略一堆
-            new SentryPlugin({
-                include: './dist',
-                release: process.env.RELEASE_VERSION,
-                configFile: 'sentry.properties',
-                urlPrefix: '~/'
-            })
-        ]
-        // module: {
-        //     rules: [
-        //         {
-        //             test: /\.js|\.vue$/,
-        //             loader: 'eslint-loader',
-        //             enforce: 'pre',
-        //             exclude: /node_modules/,
-        //             include: [path.resolve(__dirname, 'src')],
-        //             options: {
-        //                 // 这里的配置项参数将会被传递到 eslint 的 CLIEngine
-        //                 // formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
-        //                 emitWarning: true,
-        //                 emitError: false
-        //             }
-        //         }
-        //     ]
-        // }
+        plugins: getPlugins()
     }
 };
